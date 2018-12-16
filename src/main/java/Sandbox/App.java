89eb29bc9dfc2711034a2a24806303ac474656a5
zoltanmariasp92 <zoltan.marias@p92.hu>
2018-12-16 18:27:53 +0100

    gitignore

--- src/main/java/Sandbox/App.java
+++ src/main/java/Sandbox/App.java
@@ -4,11 +4,17 @@
 package Sandbox;
 
 public class App {
+
     public String getGreeting() {
         return "Hello world.";
     }
 
+    public String getStory() {
+        return "Once upon a time...";
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        System.out.println(new App().getStory());
     }
 }
